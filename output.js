51107842b6d2371035f8416064d43155b8586d74
Addy Osmani <addyosmani@gmail.com>
2014-11-01 12:39:41 +0000

    Code clean-up, output improvements

--- output.js
+++ output.js
@@ -6,118 +6,102 @@ var utils = require('./lib/utils');
 
 var averageImagesPerPage = 1200000;
 
-exports.init = function () {
-  var threshold = 70;
-  var exports = {};
+exports.init = function() {
+    var threshold = 70;
+    var exports = {};
 
-  var generateScore = function (url, strategy, score) {
-    var color = utils.scoreColor(score);
+    var generateScore = function(url, strategy, score) {
+        var color = utils.scoreColor(score);
 
-    score = 'Score:     ' + color(score);
-    url = 'URL:       ' + chalk.cyan(url);
-    strategy = 'Strategy:  ' + chalk.cyan(strategy);
+        score = 'Score:     ' + color(score);
+        url = 'URL:       ' + chalk.cyan(url);
+        strategy = 'Strategy:  ' + chalk.cyan(strategy);
 
-    return [url, score, strategy].join('\n') + '\n';
-  };
+        return [url, score, strategy].join('\n') + '\n';
+    };
 
-  var generateRuleSetResults = function (rulesets) {
-    var result, ruleImpact, title;
-    var _results = [];
+    var generateRuleSetResults = function(rulesets) {
+        var result, ruleImpact, title;
+        var _results = [];
 
-    for (title in rulesets) {
-      result = rulesets[title];
-      ruleImpact = Math.ceil(result.ruleImpact * 100) / 100;
-      _results.push(utils.labelize(title) + chalk.cyan(ruleImpact));
-    }
+        for (title in rulesets) {
+            result = rulesets[title];
+            ruleImpact = Math.ceil(result.ruleImpact * 100) / 100;
+            _results.push(utils.labelize(title) + chalk.cyan(ruleImpact));
+        }
 
-    return _results.join('\n');
-  };
+        return _results.join('\n');
+    };
 
-  var generateStatistics = function (statistics) {
-    var result, title;
-    var _results = [];
+    var generateStatistics = function(statistics) {
+        var result, title;
+        var _results = [];
 
-    console.log(averageImagesPerPage);
-    console.log(statistics.imageResponseBytes);
+        for (title in statistics) {
+            result = title.indexOf('Bytes') !== -1 ?
+                prettyBytes(+statistics[title]) :
+                statistics[title];
 
-    for (title in statistics) {
-      result = title.indexOf('Bytes') !== -1 ?
-        prettyBytes(+statistics[title]) :
-        statistics[title];
+            _results.push(utils.labelize(title) + chalk.cyan(result));
+        }
 
-      _results.push(utils.labelize(title) + chalk.cyan(result));
-    }
+        return _results.join('\n');
+    };
 
-    return _results.join('\n');
-  };
+    exports.threshold = function(limit) {
+        threshold = limit;
+        return threshold;
+    };
 
-  exports.threshold = function (limit) {
-    threshold = limit;
-    return threshold;
-  };
+    exports.process = function(parameters, response, done) {
+        var logger = console.log,
+            error = null;
 
-  exports.process = function (parameters, response, done) {
-    var logger = console.log,
-        error  = null;
+        done = done || function() {};
+        threshold = parameters.threshold || threshold;
 
-    done = done || function () {};
-    threshold = parameters.threshold || threshold;
+        var yourImageWeight = parseInt(response.pageStats.imageResponseBytes, 10);
+        var unoptimizedImages = response.formattedResults.ruleResults.OptimizeImages.urlBlocks;
+        var shave = chalk.cyan("Thanks for keeping the web fast <3");
+        var imagesToOptimize = "";
 
-    var yourImageWeight   = parseInt(response.pageStats.imageResponseBytes, 10);
-    var unoptimizedImages = response.formattedResults.ruleResults.OptimizeImages.urlBlocks;
-    var shave = chalk.cyan("Thanks for keeping the web fast <3");
+        if (yourImageWeight > averageImagesPerPage) {
+            shave = chalk.cyan('Please shave off at least:\n') + prettyBytes(yourImageWeight - averageImagesPerPage);
 
-    if (yourImageWeight > averageImagesPerPage) {
-      shave = chalk.cyan('Please shave off at least:\n') + prettyBytes(yourImageWeight - averageImagesPerPage);
-    }
+            if (unoptimizedImages[1] !== undefined) {
+                unoptimizedImages[1].urls.forEach(function(url) {
+                    url.result.args.forEach(function(x) {
+                        var result = "";
+                        switch (x.type) {
+                            case 'URL':
+                                result += chalk.green(x.value);
+                                break;
+                            case 'BYTES':
+                                result += 'Size: ' + chalk.red(x.value);
+                                break;
+                            case 'PERCENTAGE':
+                                result += 'Can be improved by ' + chalk.yellow(x.value);
+                                break;
+                        }
+                        imagesToOptimize += result + '\n'; 
+                    });
+                });
+            }
+        }
 
-    logger([
-        chalk.cyan('Your image weight:\n') + prettyBytes(yourImageWeight),
-        chalk.cyan('Average image weight on the web:\n') + prettyBytes(averageImagesPerPage),
-        shave
-      ].join('\n'));
+        logger([
+            chalk.cyan('Your image weight:\n') + prettyBytes(yourImageWeight),
+            chalk.cyan('Average image weight on the web:\n') + prettyBytes(averageImagesPerPage),
+            shave,
+            imagesToOptimize.length? (chalk.underline('\nImages to optimize:\n') + imagesToOptimize + chalk.cyan('\nThis list does not include images which cannot be optimized further.\nYou may consider removing those images if possible.')) : '', 
+        ].join('\n'));
 
-    if (unoptimizedImages[1] !== undefined) {
-      console.log(chalk.underline('\nImages to optimize:\n'));
+        if (response.score < threshold) {
+            error = new Error("Threshold of " + threshold + " not met with score of " + response.score);
+        }
 
-      unoptimizedImages[1].urls.forEach(function(url){
-        url.result.args.forEach(function(x){
+        return done(error);
+    };
 
-          var result = "";
-
-          switch (x.type) {
-            case 'URL':
-              result+= chalk.green(x.value);
-                break;
-            case 'BYTES':
-              result+= '(' + chalk.red(x.value);
-                break;
-            case 'PERCENTAGE':
-              result+= ' Can be improved by ' + chalk.yellow(x.value) + ')';
-                break;
-          }
-
-          console.log(result);
-        });
-      });
-    }
-/*
-
-    logger([
-      utils.divider,
-      generateScore(response.id, parameters.strategy, response.score),
-      generateStatistics(response.pageStats),
-      utils.labelize(''),
-      generateRuleSetResults(response.formattedResults.ruleResults),
-      utils.divider
-    ].join('\n'));*/
-
-    if (response.score < threshold) {
-      error = new Error("Threshold of " + threshold + " not met with score of " + response.score);
-    }
-
-    return done(error);
-  };
-
-  return exports;
+    return exports;
 };
