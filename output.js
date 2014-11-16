cc7518de2b4b93938f51cced10611641ea432e21
Addy Osmani <addyosmani@gmail.com>
2014-11-16 17:17:00 +0100

    Big code refactor.

--- output.js
+++ output.js
@@ -6,192 +6,212 @@ var utils = require('./lib/utils');
 var csvparse = require('csv-parse');
 var fs = require('fs');
 
+function Output() {
+
+	if (!(this instanceof Output)) {
+		return new Output;
+	}
+
+	/**
+	 * PageSpeed Insights threshold
+	 * @type {number}
+	 */
+	this.threshold = 70;
+
+	/**
+	 * What indices from BigQuery do we want to summarize scores
+	 * for in the CLI output?
+	 * @type {number[]}
+	 */
+	this.indicesOfInterest = [3, 6, 9]; // 25th, 50th, 75th percentile
+
+	/**
+	 * Include suggestions for URLs that could be improved score-wise
+	 * This will not include a complete list of possible optimizations
+	 * and relies on PageSpeed Insights API data
+	 * @type {boolean}
+	 */
+	this.verbose = false;
+
+	/**
+	 * Is the weight higher than one of the BigQuery
+	 * percentiles?
+	 * @type {boolean}
+	 */
+	this.fasterThanAPercentile = false;
+	this.bigQueryData = {
+		desktop: [], mobile: [], titles: []
+	};
+
+	/**
+	 * A subset of BigQuery data that we actually use for output
+	 * This should represent the indices of interest from the
+	 * bigQueryData.
+	 * @type {{desktop: Array, mobile: Array, titles: Array}}
+	 */
+	this.outputData = {
+		desktop: [], mobile: [], titles: []
+	};
+
+	/**
+	 * Average image weights per site, based on BigQuery data
+	 * @type {{desktop: number, mobile: number}}
+	 */
+	this.averages = {};
+
+	/**
+	 * Message shown when image weight is lower than one of the percentiles
+	 * we care about.
+	 */
+	this.keepingWebFast = chalk.cyan('Thanks for keeping the web fast <3');
 
 
-exports.init = function() {
-    var threshold = 70;
-    var indicesOfInterest = [3, 6, 9]; // 25th, 50th, 75th percentile
-    var exports = {};
-    var verbose = '';
-    var titles = [];
-    var desktop = [];
-    var mobile = [];
-    var titlesData, desktopData, mobileData = [];
-    var fasterThanAPercentile = false;
-    var desktopAverage = 0;
-    var mobileAverage = 0;
+	var parser = csvparse({
+		delimiter: ';'
+	}, function (err, data) {
 
-    var compareWeight = function(siteImageWeight, percentileImageWeight, percentile) {
-        var diff = (siteImageWeight - parseInt(percentileImageWeight, 10)) * 1000;
-        if (diff > 0) {
-            diff = chalk.red('+' + prettyBytes(diff));
-        } else {
-            diff = diff * -1;
-            diff = chalk.green('-' + prettyBytes(diff));
-            fasterThanAPercentile = true;
-        }
-        return diff + (' compared to a site in the ') + chalk.yellow(percentile.replace('p', '') + 'th') + ' percentile';
-    };
+		// TODO: Refactor all of this to be less boilerplate-y.
 
-    var highestPercentile = function(sizeImageWeight, sizes, percentiles) {
+		// Complete full set of available BigQuery Data
+		this.bigQueryData.titles = data[0][0].split(',');
+		this.bigQueryData.desktop = data[1][0].split(',');
+		this.bigQueryData.mobile = data[2][0].split(',');
 
-        var highestPercentileMatch = -1;
+		// Sub-slice portions of data we're interested
+		this.indicesOfInterest.forEach(function (item) {
+			this.outputData.desktop.push(this.bigQueryData.desktop[item]);
+			this.outputData.mobile.push(this.bigQueryData.mobile[item]);
+			this.outputData.titles.push(this.bigQueryData.titles[item]);
+		}.bind(this));
 
-        for (var i = 2; i < percentiles.length; i++) {
-            sizes[i] = parseInt(sizes[i], 10);
-            if (sizeImageWeight > sizes[i]) {
-                highestPercentileMatch = i;
-            }
-        }
+		this.averages.mobile = parseInt(this.bigQueryData.mobile[1], 10);
+		this.averages.desktop = parseInt(this.outputData.desktop[1], 10);
 
-        return percentiles[highestPercentileMatch];
-    };
+	}.bind(this));
 
-    var compareWeights = function(siteImageWeight, sizes, percentiles) {
-        var comparisons = '';
-        siteImageWeight = parseInt(siteImageWeight, 10);
-        for (var i = 0; i < percentiles.length; i++) {
-            comparisons += compareWeight(siteImageWeight, sizes[i], percentiles[i]) + '\n';
-        }
-        return comparisons;
-    };
-
-    var parser = csvparse({
-        delimiter: ';'
-    }, function(err, data) {
-
-        // Complete data set
-        titlesData = data[0][0].split(',');
-        desktopData = data[1][0].split(',');
-        mobileData = data[2][0].split(',');
-
-        // Print set
-        indicesOfInterest.forEach(function(item) {
-            desktop.push(desktopData[item]);
-            mobile.push(mobileData[item]);
-            titles.push(titlesData[item]);
-        });
-
-        // Averages
-        mobileAverage = parseInt(mobileData[1], 10);
-        desktopAverage = parseInt(desktopData[1], 10);
-
-    });
-
-    fs.createReadStream(__dirname + '/data/bigquery.csv').pipe(parser);
-
-    /*
-        var generateScore = function(url, strategy, score) {
-            var color = utils.scoreColor(score);
-
-            score = 'Score:     ' + color(score);
-            url = 'URL:       ' + chalk.cyan(url);
-            strategy = 'Strategy:  ' + chalk.cyan(strategy);
-
-            return [url, score, strategy].join('\n') + '\n';
-        };
-    */
-    /*
-        var generateRuleSetResults = function(rulesets) {
-            var result, ruleImpact, title;
-            var _results = [];
-
-            for (title in rulesets) {
-                result = rulesets[title];
-                ruleImpact = Math.ceil(result.ruleImpact * 100) / 100;
-                _results.push(utils.labelize(title) + chalk.cyan(ruleImpact));
-            }
-
-            return _results.join('\n');
-        };
-    */
-    /*
-        var generateStatistics = function(statistics) {
-            var result, title;
-            var _results = [];
-
-            for (title in statistics) {
-                result = title.indexOf('Bytes') !== -1 ?
-                    prettyBytes(+statistics[title]) :
-                    statistics[title];
-
-                _results.push(utils.labelize(title) + chalk.cyan(result));
-            }
-
-            return _results.join('\n');
-        };
-    */
-    exports.threshold = function(limit) {
-        threshold = limit;
-        return threshold;
-    };
-
-    exports.process = function(parameters, response, done) {
-        var logger = console.log,
-            error = null;
-
-        done = done || function() {};
-        threshold = parameters.threshold || threshold;
-        verbose = parameters.verbose;
-
-        var yourImageWeight = parseInt(response.pageStats.imageResponseBytes || 0, 10);
-        var unoptimizedImages = response.formattedResults.ruleResults.OptimizeImages.urlBlocks;
-        var imagesToOptimize = '';
-        var desktopWeights = compareWeights(yourImageWeight / 1000, desktop, titles);
-        var mobileWeights = compareWeights(yourImageWeight / 1000, mobile, titles);
-        // var mobileDifference = yourImageWeight - mobileAverage * 1000;
-        // var desktopDifference = yourImageWeight - desktopAverage * 1000;
-        var shave = chalk.cyan('Thanks for keeping the web fast <3');
-        var highestPercentileDesktop = highestPercentile(yourImageWeight / 1000, desktopData, titlesData);
-        var highestPercentileMobile = highestPercentile(yourImageWeight / 1000, mobileData, titlesData);
-
-        if (verbose) {
-            if (unoptimizedImages[1] !== undefined) {
-                unoptimizedImages[1].urls.forEach(function(url) {
-                    url.result.args.forEach(function(x) {
-                        var result = '';
-                        switch (x.type) {
-                            case 'URL':
-                                result += chalk.green(x.value);
-                                break;
-                            case 'BYTES':
-                                result += 'Size: ' + chalk.red(x.value);
-                                break;
-                            case 'PERCENTAGE':
-                                result += 'Can be improved by ' + chalk.yellow(x.value);
-                                break;
-                        }
-                        imagesToOptimize += result + '\n';
-                    });
-                });
-            }
-        }
-
-        logger([
-            chalk.cyan('Your image weight: ') + prettyBytes(yourImageWeight),
-
-            chalk.cyan('\nOn Mobile you are:'),
-            mobileWeights,
-            // prettyBytes(mobileDifference) + ' compared to the median site',
-            'The median site has ' + prettyBytes(mobileAverage * 1000) + ' of images',
-            'You have more image bytes than ' + highestPercentileDesktop.replace('p', '') + '% of sites',
-
-            chalk.cyan('\nOn Desktop you are:'),
-            desktopWeights,
-            'The median site has ' + prettyBytes(desktopAverage * 1000) + ' of images',
-            // prettyBytes(desktopDifference) + ' compared to the median site',
-            'You have more image bytes than ' + highestPercentileMobile.replace('p', '') + '% of sites',
-
-            fasterThanAPercentile ? shave : '',
-            imagesToOptimize.length ? (chalk.underline('\nImages to optimize:\n') + imagesToOptimize + chalk.cyan('\nThis list does not include images which cannot be optimized further.\nYou may consider removing those images if possible.')) : '',
-        ].join('\n'));
-
-        if (response.score < threshold) {
-            error = new Error('Threshold of ' + threshold + ' not met with score of ' + response.score);
-        }
-
-        return done(error);
-    };
-
-    return exports;
+	fs.createReadStream(__dirname + '/data/bigquery.csv').pipe(parser);
 };
+
+
+/**
+ * Compare a supplied image weight with the weight in a supplied percentile.
+ * @param siteImageWeight
+ * @param percentileImageWeight
+ * @param percentile
+ * @returns {string} Summary message of comparisons to the percentile
+ */
+Output.prototype.compareWeightPercentile = function (siteImageWeight, percentileImageWeight, percentile) {
+	if (siteImageWeight === undefined) {
+		return;
+	}
+	var diff = (siteImageWeight - parseInt(percentileImageWeight, 10)) * 1000;
+	if (diff > 0) {
+		diff = chalk.red('+' + prettyBytes(diff));
+	} else {
+		diff = diff * -1;
+		diff = chalk.green('-' + prettyBytes(diff));
+		this.fasterThanAPercentile = true;
+	}
+	return diff + (' compared to a site in the ') + chalk.yellow(percentile.replace('p', '') + 'th') + ' percentile';
+};
+
+Output.prototype.compareWeights = function (siteImageWeight, sizes, percentiles) {
+	if (siteImageWeight === undefined) {
+		return;
+	}
+	var comparisons = '';
+	siteImageWeight = parseInt(siteImageWeight, 10);
+	for (var i = 0; i < percentiles.length; i++) {
+		comparisons += this.compareWeightPercentile(siteImageWeight, sizes[i], percentiles[i]) + '\n';
+	}
+	return comparisons;
+};
+
+/**
+ * Check if image weight is higher than one of the available percentile sizes
+ * @param sizeImageWeight
+ * @param sizes
+ * @param percentiles
+ * @returns {*}
+ */
+Output.prototype.getHighestPercentile = function (sizeImageWeight, sizes, percentiles) {
+	if (sizeImageWeight === undefined) {
+		return;
+	}
+	var highestPercentileMatch = -1;
+	// Begin with index 2 to avoid catching unnecessary labels
+	// like `desktop` and `mobile` included in this row of data
+	for (var i = 2; i < percentiles.length; i++) {
+		sizes[i] = parseInt(sizes[i], 10);
+		if (sizeImageWeight > sizes[i]) {
+			highestPercentileMatch = i;
+		}
+	}
+	return percentiles[highestPercentileMatch];
+};
+
+
+Output.prototype.process = function (parameters, response, done) {
+
+	done = done || function () {};
+	var logger = console.log;
+	var error = null;
+	this.threshold = parameters.threshold || this.threshold;
+	this.verbose = parameters.verbose;
+	var yourImageWeight = parseInt(response.pageStats.imageResponseBytes || 0, 10);
+	var unoptimizedImages = response.formattedResults.ruleResults.OptimizeImages.urlBlocks;
+	var desktopWeights = this.compareWeights(yourImageWeight / 1000, this.outputData.desktop, this.outputData.titles);
+	var mobileWeights = this.compareWeights(yourImageWeight / 1000, this.outputData.mobile, this.outputData.titles);
+	var highestPercentileDesktop = this.getHighestPercentile(yourImageWeight / 1000, this.bigQueryData.desktop, this.bigQueryData.titles);
+	var highestPercentileMobile = this.getHighestPercentile(yourImageWeight / 1000, this.bigQueryData.mobile, this.bigQueryData.titles);
+	var imagesToOptimize = '';
+
+	if (this.verbose) {
+
+		if (unoptimizedImages[1] !== undefined) {
+			unoptimizedImages[1].urls.forEach(function (url) {
+				url.result.args.forEach(function (x) {
+					var result = '';
+					switch (x.type) {
+						case 'URL':
+							result += chalk.green(x.value);
+							break;
+						case 'BYTES':
+							result += 'Size: ' + chalk.red(x.value);
+							break;
+						case 'PERCENTAGE':
+							result += 'Can be improved by ' + chalk.yellow(x.value) + '\n';
+							break;
+					}
+					imagesToOptimize += result + '\n';
+				});
+			});
+		}
+	}
+
+	logger([
+		chalk.cyan('Your image weight: ') + prettyBytes(yourImageWeight),
+
+		chalk.cyan('\nOn Mobile you are:'),
+		mobileWeights,
+		'The median site has ' + prettyBytes(this.averages.mobile * 1000) + ' of images',
+		'You have more image bytes than ' + highestPercentileDesktop.replace('p', '') + '% of sites',
+
+		chalk.cyan('\nOn Desktop you are:'),
+		desktopWeights,
+		'The median site has ' + prettyBytes(this.averages.desktop * 1000) + ' of images',
+		'You have more image bytes than ' + highestPercentileMobile.replace('p', '') + '% of sites',
+
+		this.fasterThanAPercentile ? this.keepingWebFast: '',
+		imagesToOptimize.length ? (chalk.underline('\nImages to optimize:\n') + imagesToOptimize + chalk.cyan('\nThis list does not include images which cannot be optimized further.\nYou may consider removing those images if possible.')) : '',
+	].join('\n'));
+
+	if (response.score < this.threshold) {
+		error = new Error('Threshold of ' + this.threshold + ' not met with score of ' + response.score);
+	}
+	return done(error);
+};
+
+
+module.exports = Output;
+
